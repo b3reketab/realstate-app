f5c2f9bec3a66a30ee640525ae816e4feb36a306
Bereketab Berhanu <141371508+b3reketab@users.noreply.github.com>
2025-01-28 23:48:04 +0300

    ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,32 @@
-import "./layout.scss";
-
-import Navbar from "./components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./routes/homepage/Homepage";
+import Listpage from "./routes/ListPage/Listpage";
+import Layout from "./routes/Layout/Layout";
+import SinglePage from "./routes/SinglePage/SinglePage";
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <Homepage />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+        },
+        {
+          path: "/list",
+          element: <Listpage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        }
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/components/filter/Filter.jsx
@@ -0,0 +1,55 @@
+import "./filter.scss";
+
+function Filter() {
+  return (
+    <div className="filter">
+      <h1>
+        Search results for <b>London</b>
+      </h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="location">Location</label>
+          <input
+            type="text"
+            id="location"
+            name="location"
+            placeholder="City Location"
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select id="type" name="type">
+            <option value="">any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Poperty</label>
+          <select id="property" name="property">
+            <option value="">any</option>
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input type="number" id="minPrice" name="minPrice" placeholder="any" />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input type="number" id="maxPrice" name="maxPrice" placeholder="any" />
+        </div>
+      </div>
+      <button>
+        <img src="search.png" alt="" />
+      </button>
+    </div>
+  );
+}
+
+export default Filter;
